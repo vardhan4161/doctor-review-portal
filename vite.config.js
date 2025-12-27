42ea52874d9164c315c3f75c11c4044a50225706
vardhan4161 <vardhangoud096@gmail.com>
2025-12-27 10:10:33 +0530

    Resolve merge conflicts for remote sync

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
+=======
+  base: '/doctor-review-portal/',
+>>>>>>> b05eb52 (Initial project upload)
 });
 
